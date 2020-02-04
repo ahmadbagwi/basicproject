9ec2d77369f33303c284301637baf249870dc8a8
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2020-02-04 09:15:21 +0700

    tambah menu beranda dan ubah bahasa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,11 +41,14 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
+                                <a class="nav-link" href="{{ route('beranda') }}">{{ __('Beranda') }}</a>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Daftar') }}</a>
                                 </li>
                             @endif
                         @else
